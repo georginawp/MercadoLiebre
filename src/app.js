b174e47feef75136a01827b22039e36660101edd
Georgina Pena <georgina@MacBook-Pro-de-Georgina.local>
2022-12-14 17:01:52 -0300

    Adaptado a deploy de Render

--- src/app.js
+++ src/app.js
@@ -8,11 +8,10 @@ const path = require('path');
 });
 */
 
-
-app.listen(3030,()=>{
-    console.log ('servidor corriendo');
-});
-
+const port = process.env.PORT || 3001;
+app.listen(port,() =>
+    console.log ("Servidor corriendo en el puerto http://localhost:" + port)
+);
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
